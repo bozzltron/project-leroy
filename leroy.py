f9f79b736fb7df468d7840472455778c8b34ef26
Michael <michael.bosworth@bunchball.com>
2020-09-07 17:59:06 -0500

    add main program exception handling

--- leroy.py
+++ leroy.py
@@ -70,191 +70,196 @@ def disk_has_space():
     return hdd.percent < 95
 
 def main():
-    default_model_dir = 'all_models'
-    default_model = 'mobilenet_ssd_v2_coco_quant_postprocess_edgetpu.tflite'
-    default_labels = 'coco_labels.txt'
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--model', help='.tflite model path',
-                        default=os.path.join(default_model_dir,default_model))
-    parser.add_argument('--labels', help='label file path',
-                        default=os.path.join(default_model_dir, default_labels))
-    parser.add_argument('--top_k', type=int, default=3,
-                        help='number of categories with highest score to display')
-    parser.add_argument('--camera_idx', type=int, help='Index of which video source to use. ', default = 0)
-    parser.add_argument('--threshold', type=float, default=0.1,
-                        help='classifier score threshold')
-    args = parser.parse_args()
-    multiTracker = cv2.MultiTracker_create()
 
-    #Initialize logging files
-    logging.basicConfig(filename='storage/results.log',
-                        format='%(asctime)s-%(message)s',
-                        level=logging.DEBUG)
+    try:
+        default_model_dir = 'all_models'
+        default_model = 'mobilenet_ssd_v2_coco_quant_postprocess_edgetpu.tflite'
+        default_labels = 'coco_labels.txt'
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--model', help='.tflite model path',
+                            default=os.path.join(default_model_dir,default_model))
+        parser.add_argument('--labels', help='label file path',
+                            default=os.path.join(default_model_dir, default_labels))
+        parser.add_argument('--top_k', type=int, default=3,
+                            help='number of categories with highest score to display')
+        parser.add_argument('--camera_idx', type=int, help='Index of which video source to use. ', default = 0)
+        parser.add_argument('--threshold', type=float, default=0.1,
+                            help='classifier score threshold')
+        args = parser.parse_args()
+        multiTracker = cv2.MultiTracker_create()
 
-    print('Loading {} with {} labels.'.format(args.model, args.labels))
-    interpreter = common.make_interpreter(args.model)
-    interpreter.allocate_tensors()
-    labels = load_labels(args.labels)
+        #Initialize logging files
+        logging.basicConfig(filename='storage/results.log',
+                            format='%(asctime)s-%(message)s',
+                            level=logging.DEBUG)
 
-    cap = cv2.VideoCapture(args.camera_idx)
-        
-    #cap.set(3, 1920)
-    #cap.set(4, 1440)
-    # 4:3 resolutions
-    # 640×480, 800×600, 960×720, 1024×768, 1280×960, 1400×1050,
-    # 1440×1080 , 1600×1200, 1856×1392, 1920×1440, 2048×1536
-    # 5 MP
-    cap.set(3, 2048)
-    cap.set(4, 1536)
-    
-    bboxes = []
-    colors = [] 
-    visitation = []
-    trackers = []
-    started_tracking = None
-    last_tracked = None
-    visitation_id = None
-    recording = False
-    out = None
-    fps = FPS().start()
-    is_stopped = False
-    current_fps = 4.0
-    boxes = []
+        print('Loading {} with {} labels.'.format(args.model, args.labels))
+        interpreter = common.make_interpreter(args.model)
+        interpreter.allocate_tensors()
+        labels = load_labels(args.labels)
 
-    while cap.isOpened():
-        try:
+        cap = cv2.VideoCapture(args.camera_idx)
             
-            ret, frame = cap.read()
-            if not ret:
+        #cap.set(3, 1920)
+        #cap.set(4, 1440)
+        # 4:3 resolutions
+        # 640×480, 800×600, 960×720, 1024×768, 1280×960, 1400×1050,
+        # 1440×1080 , 1600×1200, 1856×1392, 1920×1440, 2048×1536
+        # 5 MP
+        cap.set(3, 2048)
+        cap.set(4, 1536)
+        
+        bboxes = []
+        colors = [] 
+        visitation = []
+        trackers = []
+        started_tracking = None
+        last_tracked = None
+        visitation_id = None
+        recording = False
+        out = None
+        fps = FPS().start()
+        is_stopped = False
+        current_fps = 4.0
+        boxes = []
+
+        while cap.isOpened():
+            try:
+                
+                ret, frame = cap.read()
+                if not ret:
+                    break
+
+                if fps._numFrames < 500:
+                    fps.update()
+                else:
+                    fps.stop()
+                    current_fps = fps.fps()
+                    logging.info("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+                    logging.info("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+                    fps = FPS().start()
+
+                cv2_im = frame
+                cv2_im_rgb = cv2.cvtColor(cv2_im, cv2.COLOR_BGR2RGB)
+                pil_im = Image.fromarray(cv2_im)
+
+                success, boxes = multiTracker.update(cv2_im)
+                if success:
+                    last_tracked = time.time()
+                
+                if len(boxes) > 0:
+                    logging.info("success {}".format(success))
+                    logging.info("boxes {}".format(boxes))
+
+                common.set_input(interpreter, pil_im)
+                interpreter.invoke()
+                objs = get_output(interpreter, score_threshold=args.threshold, top_k=args.top_k)
+                height, width, channels = cv2_im.shape
+                
+                bird_detected = False
+                boxes_to_draw = []
+                for obj in objs:
+                    x0, y0, x1, y1 = list(obj.bbox)
+                    x0, y0, x1, y1 = int(x0*width), int(y0*height), int(x1*width), int(y1*height)
+                    percent = int(100 * obj.score)
+                    object_label = labels.get(obj.id, obj.id)
+                    label = '{}% {}'.format(percent, object_label)
+                    
+                    if object_label == 'bird' and percent > 40:
+                        bird_detected = True
+                        new_bird = True
+                        
+                        for bbox in boxes:
+                            if intersects(bbox, obj.bbox):
+                                logging.info("intersected.. same bird")
+                                new_bird = False
+                        
+                        if new_bird and len(bboxes) == 0:
+                            logging.info("found a new bird")
+                            visitation_id =  uuid.uuid4()
+                            started_tracking = time.time()
+                            recording = True
+                            bboxes.append(obj.bbox)
+                            colors.append((randint(64, 255), randint(64, 255), randint(64, 255)))
+                            tracker = cv2.TrackerCSRT_create()
+                            trackers.append(tracker)
+                            multiTracker.add(tracker, cv2_im, obj.bbox)
+                            
+                        if disk_has_space():
+                            boxed_image_path = "storage/detected/boxed_{}_{}_{}.png".format(time.strftime("%Y-%m-%d_%H-%M-%S"), percent, visitation_id)
+                            full_image_path = "storage/detected/full_{}_{}_{}.png".format(time.strftime("%Y-%m-%d_%H-%M-%S"), percent, visitation_id)
+                            cv2.imwrite( boxed_image_path, cv2_im[y0:y1,x0:x1] )
+                            if percent > 95:
+                                cv2.imwrite( full_image_path, cv2_im ) 
+
+                        else:
+                            logging.info("Not enough disk space")
+
+                    percent = int(100 * obj.score)
+                    object_label = labels.get(obj.id, obj.id)
+                    label = '{}% {}'.format(percent, object_label)
+
+                    # postpone drawing so we don't get lines in the photos
+                    boxes_to_draw.append({
+                        "p1": (x0, y0),
+                        "p2": (x1, y1),
+                        "label": label,
+                        "label_p": (x0, y0+30)
+                    })
+
+                for box in boxes_to_draw:
+                    cv2_im = cv2.rectangle(cv2_im, box["p1"], box["p2"], (0, 255, 0), 2)
+                    cv2_im = cv2.putText(cv2_im, box["label"], box["label_p"],
+                            cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 0, 0), 2)
+
+                if recording == True and disk_has_space():
+                    if out == None:
+                        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+                        out = cv2.VideoWriter("storage/video/{}.mp4".format(visitation_id), fourcc, 4.0, (2048,1536))
+                    out.write(cv2_im_rgb)
+                    
+                if bird_detected == False and len(trackers) > 0:
+                    now = time.time()
+                    if now - last_tracked > 60:
+                        logging.info("visitation {} lasted {} seconds".format(visitation_id, now - started_tracking))
+                        logging.info("clearing trackers")
+                        for tracker in trackers:
+                            tracker.clear()
+                        multiTracker = cv2.MultiTracker_create()
+                        boxes = []
+                        colors = []
+                        trackers = []
+                        bboxes = []
+                        recording = False
+                        out.release()
+                        out = None
+
+                for i, newbox in enumerate(boxes):
+                    x0, y0, x1, y1 = list(newbox)
+                    x0, y0, x1, y1 = int(x0*width), int(y0*height), int(x1*width), int(y1*height)
+                    cv2_im = cv2.rectangle(cv2_im, (x0, y0), (x1, y1), (0, 0, 255), 2)
+                
+                cv2.namedWindow('Leroy',cv2.WINDOW_NORMAL)
+                cv2.resizeWindow('Leroy', 800, 600)
+                cv2.imshow('Leroy', cv2_im)
+
+            except KeyboardInterrupt:
+                print('Interrupted')
+                try:
+                    sys.exit(0)
+                except SystemExit:
+                    os._exit(0)
+            except:
+                logging.exception('Failed while looping.')
+            if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
-            if fps._numFrames < 500:
-                fps.update()
-            else:
-                fps.stop()
-                current_fps = fps.fps()
-                logging.info("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
-                logging.info("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
-                fps = FPS().start()
+        cap.release()
+        cv2.destroyAllWindows()
+        out.release()
+        
+    except: 
+        logging.exception('Failed on main program.')
 
-            cv2_im = frame
-            cv2_im_rgb = cv2.cvtColor(cv2_im, cv2.COLOR_BGR2RGB)
-            pil_im = Image.fromarray(cv2_im)
-
-            success, boxes = multiTracker.update(cv2_im)
-            if success:
-                last_tracked = time.time()
-            
-            if len(boxes) > 0:
-                logging.info("success {}".format(success))
-                logging.info("boxes {}".format(boxes))
-
-            common.set_input(interpreter, pil_im)
-            interpreter.invoke()
-            objs = get_output(interpreter, score_threshold=args.threshold, top_k=args.top_k)
-            height, width, channels = cv2_im.shape
-            
-            bird_detected = False
-            boxes_to_draw = []
-            for obj in objs:
-                x0, y0, x1, y1 = list(obj.bbox)
-                x0, y0, x1, y1 = int(x0*width), int(y0*height), int(x1*width), int(y1*height)
-                percent = int(100 * obj.score)
-                object_label = labels.get(obj.id, obj.id)
-                label = '{}% {}'.format(percent, object_label)
-                
-                if object_label == 'bird' and percent > 40:
-                    bird_detected = True
-                    new_bird = True
-                    
-                    for bbox in boxes:
-                        if intersects(bbox, obj.bbox):
-                            logging.info("intersected.. same bird")
-                            new_bird = False
-                    
-                    if new_bird and len(bboxes) == 0:
-                        logging.info("found a new bird")
-                        visitation_id =  uuid.uuid4()
-                        started_tracking = time.time()
-                        recording = True
-                        bboxes.append(obj.bbox)
-                        colors.append((randint(64, 255), randint(64, 255), randint(64, 255)))
-                        tracker = cv2.TrackerCSRT_create()
-                        trackers.append(tracker)
-                        multiTracker.add(tracker, cv2_im, obj.bbox)
-                        
-                    if disk_has_space():
-                        boxed_image_path = "storage/detected/boxed_{}_{}_{}.png".format(time.strftime("%Y-%m-%d_%H-%M-%S"), percent, visitation_id)
-                        full_image_path = "storage/detected/full_{}_{}_{}.png".format(time.strftime("%Y-%m-%d_%H-%M-%S"), percent, visitation_id)
-                        cv2.imwrite( boxed_image_path, cv2_im[y0:y1,x0:x1] )
-                        if percent > 95:
-                            cv2.imwrite( full_image_path, cv2_im ) 
-
-                    else:
-                        logging.info("Not enough disk space")
-
-                percent = int(100 * obj.score)
-                object_label = labels.get(obj.id, obj.id)
-                label = '{}% {}'.format(percent, object_label)
-
-                # postpone drawing so we don't get lines in the photos
-                boxes_to_draw.append({
-                    "p1": (x0, y0),
-                    "p2": (x1, y1),
-                    "label": label,
-                    "label_p": (x0, y0+30)
-                })
-
-            for box in boxes_to_draw:
-                cv2_im = cv2.rectangle(cv2_im, box["p1"], box["p2"], (0, 255, 0), 2)
-                cv2_im = cv2.putText(cv2_im, box["label"], box["label_p"],
-                        cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 0, 0), 2)
-
-            if recording == True and disk_has_space():
-                if out == None:
-                    fourcc = cv2.VideoWriter_fourcc(*'DIVX')
-                    out = cv2.VideoWriter("storage/video/{}.mp4".format(visitation_id), fourcc, 4.0, (2048,1536))
-                out.write(cv2_im_rgb)
-                
-            if bird_detected == False and len(trackers) > 0:
-                now = time.time()
-                if now - last_tracked > 60:
-                    logging.info("visitation {} lasted {} seconds".format(visitation_id, now - started_tracking))
-                    logging.info("clearing trackers")
-                    for tracker in trackers:
-                        tracker.clear()
-                    multiTracker = cv2.MultiTracker_create()
-                    boxes = []
-                    colors = []
-                    trackers = []
-                    bboxes = []
-                    recording = False
-                    out.release()
-                    out = None
-
-            for i, newbox in enumerate(boxes):
-                x0, y0, x1, y1 = list(newbox)
-                x0, y0, x1, y1 = int(x0*width), int(y0*height), int(x1*width), int(y1*height)
-                cv2_im = cv2.rectangle(cv2_im, (x0, y0), (x1, y1), (0, 0, 255), 2)
-            
-            cv2.namedWindow('Leroy',cv2.WINDOW_NORMAL)
-            cv2.resizeWindow('Leroy', 800, 600)
-            cv2.imshow('Leroy', cv2_im)
-
-        except KeyboardInterrupt:
-            print('Interrupted')
-            try:
-                sys.exit(0)
-            except SystemExit:
-                os._exit(0)
-        except:
-            logging.exception('Something happened.')
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()
-    out.release()
-    
 if __name__ == '__main__':
     main()
