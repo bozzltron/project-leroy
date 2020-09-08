77317f04c99bff1acb6de3662e30f1b2c510e719
Michael <michael.bosworth@bunchball.com>
2020-09-07 19:04:00 -0500

    switch encoding

--- classify.sh
+++ classify.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 sudo systemctl stop leroy.service
+sleep 1
 python3 classify.py --dir=storage/detected
+sleep 1
 #rm -rf storage/detected
 sudo systemctl start leroy.service
--- leroy.py
+++ leroy.py
@@ -214,8 +214,8 @@ def main():
 
                 if recording == True and disk_has_space():
                     if out == None:
-                        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-                        out = cv2.VideoWriter("storage/video/{}.mp4".format(visitation_id), fourcc, 4.0, (2048,1536))
+                        fourcc = cv2.VideoWriter_fourcc(*'XVID')
+                        out = cv2.VideoWriter("storage/video/{}.avi".format(visitation_id), fourcc, 4.0, (2048,1536))
                     out.write(cv2_im_rgb)
                     
                 if bird_detected == False and len(trackers) > 0:
@@ -257,7 +257,7 @@ def main():
         cap.release()
         cv2.destroyAllWindows()
         out.release()
-        
+
     except: 
         logging.exception('Failed on main program.')
 
