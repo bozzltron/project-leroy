439e8cff09d68797f5e781c8e58fa7fc9ffc061f
Michael <michael.bosworth@bunchball.com>
2020-09-08 17:03:09 -0500

    try gstreamer again

--- leroy.py
+++ leroy.py
@@ -95,7 +95,8 @@ def main():
         interpreter.allocate_tensors()
         labels = load_labels(args.labels)
 
-        cap = cv2.VideoCapture(args.camera_idx, cv2.CAP_V4L2)
+        cap = cv2.VideoCapture('videotestsrc ! video/x-raw,framerate=20/1 ! videoscale ! videoconvert ! appsink', cv2.CAP_GSTREAMER)
+        #cap = cv2.VideoCapture(args.camera_idx, cv2.CAP_V4L2)
             
         #cap.set(3, 1920)
         #cap.set(4, 1440)
