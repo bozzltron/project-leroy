2fda6e727443ea705f8043b578b8c970122b10f7
Michael <michael.bosworth@bunchball.com>
2020-09-12 09:18:49 -0500

    run leroy

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-python3 -m venv .
-pip3 install https://dl.google.com/coral/python/tflite_runtime-2.1.0.post1-cp37-cp37m-linux_armv7l.whl
-pip3 install opencv-python opencv-contrib-python image imutils psutil
+#python3 -m venv .
+#pip3 install https://dl.google.com/coral/python/tflite_runtime-2.1.0.post1-cp37-cp37m-linux_armv7l.whl
+#pip3 install opencv-python 
+pip3 install opencv-contrib-python image imutils psutil
+sudo cp service/leroy.service /lib/systemd/system/leroy.service
+sudo chmod 644 /lib/systemd/system/leroy.service
+sudo systemctl daemon-reload
+sudo systemctl enable leroy.service
 cd all_models
 wget https://github.com/google-coral/edgetpu/raw/master/test_data/mobilenet_v2_1.0_224_inat_bird_quant_edgetpu.tflite
 wget https://github.com/google-coral/edgetpu/raw/master/test_data/inat_bird_labels.txt
