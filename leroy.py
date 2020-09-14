3aaa5dfb4db99ae61ee1d9c08ace1d470708fbc2
Michael <michael.bosworth@bunchball.com>
2020-09-14 12:51:34 -0500

    run leroy as a daemon

--- leroy.py
+++ leroy.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import argparse
 import collections
 import common
@@ -17,9 +18,9 @@ from edgetpu.utils import dataset_utils
 from random import randint
 from imutils.video import FPS
 from imutils.video import WebcamVideoStream
+from visitations import Visitations
 
 print("cv version" + cv2.__version__)
-print(cv2.getBuildInformation())
 
 Object = collections.namedtuple('Object', ['id', 'score', 'bbox'])
 
@@ -54,26 +55,6 @@ def get_output(interpreter, score_threshold, top_k, image_scale=1.0):
                       ymax=np.minimum(1.0, ymax)))
 
     return [make(i) for i in range(top_k) if scores[i] >= score_threshold]
-
-def intersects(box1, box2):
-    logging.info("box1 {}".format(box1))
-    logging.info("box2 {}".format(box2))
-    box1x0, box1y0, box1x1, box1y1 = list(box1)
-    box2x0, box2y0, box2x1, box2y1 = list(box2)
-    return not (box1x0 < box2x1 or box1x1 > box2x0 or box1y0 < box2y1 or box1y1 > box2y0)
-
-def disk_has_space():
-    hdd = psutil.disk_usage('/')
-    return hdd.percent < 95
-
-def clarity(image):
-    # compute the Laplacian of the image and then return the focus
-    # measure, which is simply the variance of the Laplacian
-  gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-  return 0 if image is None else cv2.Laplacian(gray, cv2.CV_64F).var()
-
-def is_focused(image):
-    return clarity(image) > 100
 
 def main():
 
@@ -116,23 +97,12 @@ def main():
         cap.set(3, 2048)
         cap.set(4, 1536)
         
-        bboxes = []
-        colors = [] 
-        visitation = []
-        trackers = []
-        started_tracking = None
-        last_tracked = None
-        visitation_id = None
-        recording = False
         out = None
         fps = FPS().start()
         is_stopped = False
         current_fps = 4.0
-        boxes = []
-        photo_per_visitation_count = 0
-        photo_per_visitation_max = 10
-        full_photo_per_visitation_max = 1
-        full_photo_per_visitation_count = 0
+
+        visitations = Visitations()
 
         while cap.isOpened():
             try:
@@ -154,117 +124,12 @@ def main():
                 cv2_im_rgb = cv2.cvtColor(cv2_im, cv2.COLOR_BGR2RGB)
                 pil_im = Image.fromarray(cv2_im)
 
-                success, boxes = multiTracker.update(cv2_im)
-                if success:
-                    last_tracked = time.time()
-                
-                if len(boxes) > 0:
-                    logging.info("success {}".format(success))
-                    logging.info("boxes {}".format(boxes))
-
                 common.set_input(interpreter, pil_im)
                 interpreter.invoke()
                 objs = get_output(interpreter, score_threshold=args.threshold, top_k=args.top_k)
                 height, width, channels = cv2_im.shape
                 
-                bird_detected = False
-                boxes_to_draw = []
-                for obj in objs:
-                    x0, y0, x1, y1 = list(obj.bbox)
-                    x0, y0, x1, y1 = int(x0*width), int(y0*height), int(x1*width), int(y1*height)
-                    percent = int(100 * obj.score)
-                    object_label = labels.get(obj.id, obj.id)
-                    label = '{}% {}'.format(percent, object_label)
-                    
-                    if object_label == 'bird' and percent > 20:
-                        bird_detected = True
-                        new_bird = True
-                        
-                        for bbox in boxes:
-                            if intersects(bbox, obj.bbox):
-                                logging.info("intersected.. same bird")
-                                new_bird = False
-                        
-                        if new_bird and len(bboxes) == 0:
-                            logging.info("found a new bird")
-                            visitation_id =  uuid.uuid4()
-                            started_tracking = time.time()
-                            recording = True
-                            bboxes.append(obj.bbox)
-                            colors.append((randint(64, 255), randint(64, 255), randint(64, 255)))
-                            tracker = cv2.TrackerCSRT_create()
-                            trackers.append(tracker)
-                            print('bbox {}'.format(obj.bbox))
-                            width = obj.bbox.xmax-obj.bbox.xmin
-                            height = obj.bbox.ymax-obj.bbox.ymin
-                            multiTracker.add(tracker, cv2_im, (obj.bbox.xmin, obj.bbox.ymin, width/2, height/2))
-                            
-                        if disk_has_space() and photo_per_visitation_count <= photo_per_visitation_max:
-                            directory = "storage/detected/{}/{}".format(time.strftime("%Y-%m-%d"), visitation_id)
-                            if not os.path.exists(directory):
-                                os.makedirs(directory)
-                            boxed_image_path = "{}/boxed_{}_{}.png".format(directory, time.strftime("%H-%M-%S"), percent)
-                            cv2.imwrite( boxed_image_path, cv2_im[y0:y1,x0:x1] )
-                            photo_per_visitation_count = photo_per_visitation_count + 1
-
-                        else:
-                            logging.info("Not enough disk space")
-
-                    percent = int(100 * obj.score)
-                    object_label = labels.get(obj.id, obj.id)
-                    label = '{}% {}'.format(percent, object_label)
-
-                    # postpone drawing so we don't get lines in the photos
-                    boxes_to_draw.append({
-                        "p1": (x0, y0),
-                        "p2": (x1, y1),
-                        "label": label,
-                        "label_p": (x0, y0+30)
-                    })
-
-                for box in boxes_to_draw:
-                    if box["label"] == "bird":
-                        cv2_im = cv2.rectangle(cv2_im, box["p1"], box["p2"], (169, 68, 66), 5)
-                        cv2_im = cv2.putText(cv2_im, box["label"], box["label_p"], cv2.FONT_HERSHEY_SIMPLEX, 2.0, (169, 68, 66), 5)
-
-                if disk_has_space() and full_photo_per_visitation_count <= full_photo_per_visitation_max:
-                    directory = "storage/detected/{}/{}".format(time.strftime("%Y-%m-%d"), visitation_id)
-                    if not os.path.exists(directory):
-                        os.makedirs(directory)
-                    full_image_path = "{}/full_{}.png".format(directory, time.strftime("%H-%M-%S"))
-                    full_photo_per_visitation_count = full_photo_per_visitation_count + 1
-                    cv2.imwrite( full_image_path, cv2_im ) 
-
-                # if recording == True and disk_has_space():
-                #     if out == None:
-                #         fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-                #         out = cv2.VideoWriter("storage/video/{}.mp4".format(visitation_id), fourcc, 4.0, (2048,1536))
-                #         #out = cv2.VideoWriter('appsrc ! videoconvert ! x264enc tune=zerolatency bitrate=500 speed-preset=superfast ! rtph264pay ! udpsink host=127.0.0.1 port=5000',cv2.CAP_GSTREAMER,0, 20, (2048,1536), True)
-                #     out.write(cv2_im)
-                    
-                if bird_detected == False and len(trackers) > 0:
-                    now = time.time()
-                    if now - last_tracked > 60:
-                        logging.info("visitation {} lasted {} seconds".format(visitation_id, now - started_tracking))
-                        logging.info("clearing trackers")
-                        for tracker in trackers:
-                            tracker.clear()
-                        multiTracker = cv2.MultiTracker_create()
-                        boxes = []
-                        colors = []
-                        trackers = []
-                        bboxes = []
-                        photo_per_visitation_count = 0
-                        full_photo_per_visitation_count = 0
-                        recording = False
-                        if out is not None:
-                            out.release()
-                            out = None
-
-                for i, newbox in enumerate(boxes):
-                    x0, y0, x1, y1 = list(newbox)
-                    x0, y0, x1, y1 = int(x0*width), int(y0*height), int(x1*width), int(y1*height)
-                    cv2_im = cv2.rectangle(cv2_im, (x0, y0), (x1, y1), (0, 0, 255), 2)
+                visitations.update(objs, cv2_im, labels)
                 
                 cv2.namedWindow('Leroy',cv2.WINDOW_NORMAL)
                 cv2.resizeWindow('Leroy', 800, 600)
