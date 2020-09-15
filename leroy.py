cd5dc8a18957aaa64a93bdfe3bd38d2684ed4efe
Michael <michael.bosworth@bunchball.com>
2020-09-14 21:13:32 -0500

    revert leroy

--- leroy.py
+++ leroy.py
@@ -73,7 +73,6 @@ def main():
         parser.add_argument('--threshold', type=float, default=0.1,
                             help='classifier score threshold')
         args = parser.parse_args()
-        multiTracker = cv2.MultiTracker_create()
 
         #Initialize logging files
         logging.basicConfig(filename='storage/results.log',
@@ -124,8 +123,8 @@ def main():
                     logging.info("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
                     fps = FPS().start()
 
-                cv2_im = frame.copy()
-                imutils.resize(cv2_im, width=500)
+                cv2_im = frame
+                #imutils.resize(cv2_im, width=500)
                 cv2_im_rgb = cv2.cvtColor(cv2_im, cv2.COLOR_BGR2RGB)
                 pil_im = Image.fromarray(cv2_im)
 
@@ -134,11 +133,11 @@ def main():
                 objs = get_output(interpreter, score_threshold=args.threshold, top_k=args.top_k)
                 height, width, channels = cv2_im.shape
                 
-                visitations.update(objs, frame, labels)
+                visitations.update(objs, cv2_im, labels)
                 
                 #cv2.namedWindow('Leroy',cv2.WINDOW_NORMAL)
                 #cv2.resizeWindow('Leroy', 800, 600)
-                cv2.imshow('Leroy', frame)
+                cv2.imshow('Leroy', cv2_im)
 
             except KeyboardInterrupt:
                 print('Interrupted')
