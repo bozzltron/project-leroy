2bac9ded41187d5fab3478fa778b5680ce7daa29
Michael <michael.bosworth@bunchball.com>
2020-09-16 16:18:58 -0500

    experiment

--- leroy-edgetpu.py
+++ leroy-edgetpu.py
@@ -116,6 +116,8 @@ def main():
   # initialize the video stream and allow the camera sensor to warmup
   print("[INFO] starting video stream...")
   vs = VideoStream(src=0, usePiCamera=True, resolution=(3264, 2448)).start()
+  vs.stream.set(3, 3264)
+  vs.stream.set(4, 2448)
   #vs = VideoStream(usePiCamera=False).start()
   time.sleep(2.0)
 
