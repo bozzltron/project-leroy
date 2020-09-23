7356df0683730f40bb3ff3d10ce4c672d926d2d7
Michael <michael.bosworth@bunchball.com>
2020-09-23 07:17:55 -0500

    use leroy

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 git pull origin master
 #pip3 install -r requirements.txt
 make set_resolution
-./leroy-edgetpu.py
+./leroy.py
 #python3 two_models.py
 #docker run --privileged --device /dev/video0 -v `pwd`/storage:/usr/src/app/storage -p 5005:5005 -v /dev/bus/usb:/dev/bus/usb michaelbosworth/project-leroy:latest two_models.py
 #docker run michaelbosworth/project-leroy:latest two_models.py
