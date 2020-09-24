b2b6521706ed98020cda3e4710d6479babe89c3a
Michael <michael.bosworth@bunchball.com>
2020-09-23 22:12:54 -0500

    built assets for now

---
+++ web/build/precache-manifest.59feecedad3708590895e30938ed1f17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84c45e6e19b8242a7ac112b63e6cf1b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b632dfbd2a5d752dfe0f",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2336547c09dac680822d",
+    "url": "/static/js/2.82307e37.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.82307e37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b632dfbd2a5d752dfe0f",
+    "url": "/static/js/main.94407219.chunk.js"
+  },
+  {
+    "revision": "8807eb01aff89b649bba",
+    "url": "/static/js/runtime-main.19749551.js"
+  }
+]);
