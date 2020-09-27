7558173de99f4b238461984bb0b70148fe00760b
Michael <michael.bosworth@bunchball.com>
2020-09-27 12:22:12 -0500

    new html

---
+++ web/build/precache-manifest.59410a37fda9c7f4964a2ff1b65fb377.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07e3d335161d845070b47754b7a5277c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d21e5396f8614f0cdc9",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c0112686d578278de071",
+    "url": "/static/js/2.bd2768fd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.bd2768fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d21e5396f8614f0cdc9",
+    "url": "/static/js/main.ab9e8848.chunk.js"
+  },
+  {
+    "revision": "8807eb01aff89b649bba",
+    "url": "/static/js/runtime-main.19749551.js"
+  }
+]);
