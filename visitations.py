c718011ed43c519b9d47b5d78a7ca2280fdaefce
Michael <michael.bosworth@bunchball.com>
2020-10-08 07:59:06 -0500

    adjust threshold

--- visitations.py
+++ visitations.py
@@ -49,7 +49,7 @@ class Visitations:
 
             label = '{}% {}'.format(percent, object_label)
             
-            if object_label == 'bird' and percent > 50:
+            if object_label == 'bird' and percent > 45:
                 bird_detected = True
                 
                 if self.visitation_id == None:
