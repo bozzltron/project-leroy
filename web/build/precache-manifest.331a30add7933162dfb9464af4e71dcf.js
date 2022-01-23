bbe812e321a2ed4b8987c652440f0aa855ef3b49
Michael Bosworth <mbosworth@gridrival.com>
2022-01-23 13:35:48 -0700

    web update

---
+++ web/build/precache-manifest.331a30add7933162dfb9464af4e71dcf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "756bde82d36c88981e254569d554193e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f933bbbb61a7e0f3e532",
+    "url": "/static/css/main.629ddae0.chunk.css"
+  },
+  {
+    "revision": "b586ce70a6d18ff6ab83",
+    "url": "/static/js/2.bffa3e08.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.bffa3e08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f933bbbb61a7e0f3e532",
+    "url": "/static/js/main.077f413e.chunk.js"
+  },
+  {
+    "revision": "8807eb01aff89b649bba",
+    "url": "/static/js/runtime-main.19749551.js"
+  }
+]);
